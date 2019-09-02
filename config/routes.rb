04cf223afb76bf3ffe2e213cdc1ea828ede249ec
Flavia Muntean <muntean.flavia@gmail.com>
2019-09-02 19:23:27 +0100

    styled first page and sign-in sign-up forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,20 @@ Rails.application.routes.draw do
       controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
+  get '/home', to: 'users#index', as: :home
+  # show page for user allowing creation of videos and audio files
   resources :users, only: [:show] do
     resources :videos, only: [:create]
     resources :audios, only: [:create]
+  end
+
+  # show page for user with audio, bio and influences routes
+  resources :users do
+    member do
+      get 'audio'
+      get 'bio'
+      get 'influences'
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
