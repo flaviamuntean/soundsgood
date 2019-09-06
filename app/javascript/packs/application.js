9ff14a893cb5eae08baf75029fcf0702b587b0b5
Tiago Faria <tiago.vazfaria14@gmail.com>
2019-09-06 13:40:46 +0100

    modal done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,10 @@
 import "bootstrap";
 
 import {initGeoLocation} from "components/location.js";
+import {submitAll} from "../components/modal.js";
 import {addPhoto} from "../components/photo.js";
-
 import {myFunction} from"../components/profile-navbar.js";
 initGeoLocation();
 addPhoto();
+submitAll();
 myFunction();
