0ed69bc4cca55feb1c165a72fccbe9f43c1b28bf
Tiago Faria <tiago.vazfaria14@gmail.com>
2019-09-09 11:49:53 +0100

    fixed modal jv, i think...

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
       get 'add-to-playlist', as: :add_to_playlist
       patch 'update-instrument', to: "users#update_instrument", as: :update_instrument
       patch 'update-genre', to: "users#update_genre", as: :update_genre
+      patch 'update_profile', to: "users#update_profile", as: :update_profile
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
