68fafb88130830e45a9806f2a07752f0e754b870
Flavia Muntean <muntean.flavia@gmail.com>
2019-09-10 10:06:18 +0100

    migration deleted

--- db/migrate/20190909152728_add_dates_to_conversations.rb
+++
@@ -1,6 +0,0 @@
-class AddDatesToConversations < ActiveRecord::Migration[5.2]
-  def change
-    add_column :conversations, :created_at, :datetime, null: false
-    add_column :conversations, :updated_at, :datetime, null: false
-  end
-end
