5108529e0a6c222d09d4842a68727dc54d6fa7d0
Bruno Nery <bpnery13@gmail.com>
2019-09-12 10:25:46 +0100

    meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,8 @@ class ApplicationController < ActionController::Base
   # def after_sign_up_path_for(resource)
   #   user_path(resource)
   # end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
